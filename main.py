e4822e33f92ee3d7922c620f3293127162ddfe56
KarimKamaletdinov <65394332+KarimKamaletdinov@users.noreply.github.com>
2023-02-09 19:20:19 +0500

    fix right button & fps

--- main.py
+++ main.py
@@ -17,52 +17,41 @@ font = pygame.font.SysFont('Comic Sans MS', 30)
 
 while run:
 
-    clock.tick(60)
+    milliseconds = clock.tick(128)
 
     canvas.fill((255, 255, 255))
-
-
-    """
-    milliseconds = clock.tick()
     seconds = milliseconds / 1000
-    if seconds != 0:
-        text_surface = font.render(str(1 / seconds), False, (0, 0, 0))
-        canvas.blit(text_surface, (10, 10))
     if pygame.key.get_pressed() [pygame.K_RIGHT]:
-        x += 1
+        x += 10
     if pygame.key.get_pressed() [pygame.K_LEFT]:
-        x -= 1
+        x -= 10
     if pygame.key.get_pressed() [pygame.K_UP]:
-        y -= 1
+        y -= 10
     if pygame.key.get_pressed() [pygame.K_DOWN]:
-        y += 1
-    """
+        y += 10
+
     f = pygame.mouse.get_pressed()
     gps = pygame.mouse.get_pos()
 
     if f[0]:
         if gps[0] >= 60 and gps[0] <= 120 and gps[1] >= 20 and gps[1] <= 70:
             y = y - 10
-
-    if f[0]:
         if gps[0] >= 10 and gps[0] <= 70 and gps[1] >= 80 and gps[1] <= 130:
             x = x - 10
-
-    if f[0]:
-        if gps[0] >= 110 and gps[0] <= 170 and gps[1] >= 60 and gps[1] <= 110:
+        if gps[0] >= 110 and gps[0] <= 170 and gps[1] >= 80 and gps[1] <= 130:
             x = x + 10
-
-    if f[0]:
         if gps[0] >= 60 and gps[0] <= 120 and gps[1] >= 150 and gps[1] <= 200:
             y = y + 10
-
-
 
     pygame.draw.rect(canvas, (255, 0, 0), (60, 20, 60, 50))
     pygame.draw.rect(canvas, (255, 0, 0), (10, 80, 60, 50))
     pygame.draw.rect(canvas, (255, 0, 0), (110, 80, 60, 50))
     pygame.draw.rect(canvas, (255, 0, 0), (60, 150, 60, 50))
     pygame.draw.circle(canvas, (0, 130, 0), (x, y), 70)
+
+    if seconds != 0:
+        text_surface = font.render(str(int(1 // seconds)), False, (0, 0, 0))
+        canvas.blit(text_surface, (10, 10))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
